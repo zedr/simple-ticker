d5a9e65ec0fa8a042b8747efe6bba108b490d549
Rigel Di Scala <rdiscala@redhat.com>
2019-11-17 21:39:41 +0100

    Flush everything immediately

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ LOGFILE_PATH = os.environ.get('SIMPLE_LOGFILE_PATH', '/var/log/simple.log')
 def main():
     with open(LOGFILE_PATH, 'w') as fd:
         while True:
-            print(dt.datetime.now().isoformat(), file=fd)
+            print(dt.datetime.now().isoformat(), file=fd, flush=True)
             time.sleep(10)
 
 if __name__ == "__main__":
